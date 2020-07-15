7b912f841f7c153bcc84f48a111213f1a052c454
Peter Leyva <peter.leyva.bazan@gmail.com>
2020-07-15 10:49:37 -0700

    Remove

--- util/database.js
+++ util/database.js
@@ -1,19 +1,5 @@
 const mysql = require('mysql2');
 
-// const config = {
-// 	host: "localhost",
-// 	user: "root",
-// 	password: "ff2410pets",
-// 	database: "wotv_local",
-// 	connectionLimit: 5,
-// 	connectTimeout: 10000, // 10 seconds
-// 	acquireTimeout: 10000, // 10 seconds
-// 	waitForConnections: true, // Default: true
-// 	queueLimit: 0,
-// };
-// const pool = mysql.createPool(config);
-// pool.query = util.promisify(pool.query);
-// module.exports = pool;
 const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
